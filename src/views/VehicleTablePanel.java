2cc26196b5f8be797b52e14e31e1008a2f91eeee
haezriel <michael.allen-bond@email.wsu.edu>
2014-12-01 15:40:02 -0800

    hrm

--- src/views/VehicleTablePanel.java
+++ src/views/VehicleTablePanel.java
@@ -88,26 +88,6 @@ public class VehicleTablePanel extends JPanel {
         System.out.println("--------------------------");
     }
 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-   /*void createAndShowGUI(VehicleController vController) {
-        //Create and set up the window.
-        JFrame frame = new JFrame("SimpleTableDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Create and set up the content pane.
-        VehicleTablePanel newContentPane = new VehicleTablePanel(vList);
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(newContentPane);
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }*/
-
 	public void addSubmitButtonListener(ActionListener listenerForSubmitButton) 
 	{
 		this.btnSubmit.addActionListener(listenerForSubmitButton);

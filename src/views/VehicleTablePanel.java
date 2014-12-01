e1ea6b3db61390d0d05211fadd89dd9c7836c5c3
haezriel <michael.allen-bond@email.wsu.edu>
2014-12-01 13:17:19 -0800

    Pushing again, hopefully this doesn't get deleted again, but if it does, I'm backing it up just in case.

--- src/views/VehicleTablePanel.java
+++ src/views/VehicleTablePanel.java
@@ -16,9 +16,15 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import javax.swing.BoxLayout;
+import java.awt.Component;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.FlowLayout;
 
 public class VehicleTablePanel extends JPanel {
     private boolean DEBUG = false;
+    public JButton btnEdit;
+    public JButton btnAddNewCar;
     public JButton btnSubmit;
     ArrayList<VehiclesObject> vehiclelist;
     VehicleController vController;
@@ -26,59 +32,44 @@ public class VehicleTablePanel extends JPanel {
 
     public VehicleTablePanel(ArrayList <VehiclesObject> vlist) {
         
-    //    Object[][] data = null;
-     
+    	ArrayList<Object[]> data = new ArrayList<Object[]>();
+    	int i=0;
+    	for(VehiclesObject v:vlist){
+    		data.add(new Object[]{v.idvehicle, v.make, v.model, v.color, v.licensePlate, v.mileage});
+    	}
+    	
         String[] columnNames = {"Color",
                                 "License Plate",
                                 "Vehicle ID",
                                 "Make",
                                 "Model",
                                 "Mileage"};
+        
 
-//        vehiclelist = vlist;
-//        System.out.println(vehiclelist);
-//		int i=0;
-//		for (VehiclesObject v:vehiclelist)
-//		{
-//			data[i][0]=v.color;
-//			data[i][1]=v.licensePlate;
-//			data[i][2]=v.idvehicle;
-//			data[i][3]=v.make;
-//			data[i][4]=v.model;
-//			data[i][5]=v.mileage;
-//			i++;
-//			System.out.println(v.color + v.licensePlate + v.idvehicle + v.make + v.model + v.mileage);
-//		}
-		
-		Object[][] data = {{"Blue", "MMR 234", "3", "GM", "Truck", "45602"},
-				{"White", "SWV 123", "1", "Toyota", "Camry", "43100"},
-				{"Red", "FTT 123", "2", "Toyota", "Corolla", "34500"},
-				{"Black", "TR9472", "4", "Tesla", "S", "25300"}
-		};
-		
+        
+        JTable table = new JTable(new VehicleTableModel(data, columnNames));
 
-        final JTable table = new JTable(data, columnNames);
-        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+	
+        table.setPreferredScrollableViewportSize(new Dimension(500, 500));
         table.setFillsViewportHeight(true);
 
         if (DEBUG) {
             table.addMouseListener(new MouseAdapter() {
                 public void mouseClicked(MouseEvent e) {
-                    printDebugData(table);
                 }
             });
         }
-        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
         //Create the scroll pane and add the table to it.
         JScrollPane scrollPane = new JScrollPane(table);
-
-        //Add the scroll pane to this panel.
-        add(scrollPane);
         
-		btnSubmit = new JButton("Go to next screen");
-		btnSubmit.setBounds(10, 220, 95, 29);
+        btnSubmit = new JButton("Go to next screen");
+		
+		btnAddNewCar = new JButton("Add New Car");
+		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		add(scrollPane);
 		add(btnSubmit);
+		add(btnAddNewCar);
     }
 
     private void printDebugData(JTable table) {
@@ -106,17 +97,15 @@ public class VehicleTablePanel extends JPanel {
         //Create and set up the window.
         JFrame frame = new JFrame("SimpleTableDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         //Create and set up the content pane.
         VehicleTablePanel newContentPane = new VehicleTablePanel(vList);
         newContentPane.setOpaque(true); //content panes must be opaque
         frame.setContentPane(newContentPane);
-
         //Display the window.
         frame.pack();
         frame.setVisible(true);
     }*/
-
+    
 	public void addSubmitButtonListener(ActionListener listenerForSubmitButton) 
 	{
 		this.btnSubmit.addActionListener(listenerForSubmitButton);
